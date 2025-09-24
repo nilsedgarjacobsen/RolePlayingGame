75af3101eebba6cd8a9590a186f2b86b1336669b
nilsedgar <nils.jacobsen@zocom.se>
2025-09-24 10:25:43 +0200

    added gamecharacter superclass

---
+++ src/GameCharacter.java
@@ -0,0 +1,21 @@
+public abstract class GameCharacter {
+
+    private int healthPoints;
+    private int attackValue = 10;
+
+    public GameCharacter(int healthPoints){
+        this.healthPoints = healthPoints;
+    }
+
+    public int attack(){
+        return this.attackValue;
+    }
+
+    public int getHealthPoints() {
+        return healthPoints;
+    }
+
+    public void setHealthPoints(int healthPoints) {
+        this.healthPoints = healthPoints;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         //Klasser jag vill ha:
-        //GameCharacter
+        //GameCharacter(superklass), Hero, Enemy, Weapon, Armor
+        //Healthpoints, attack(), vapen och skydd (composition)
     }
 }
